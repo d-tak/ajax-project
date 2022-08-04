72b0c2e5f5b0d8d9305386948c7b8a9091eeb826
Daniel Tak <danieljtak@hotmail.com>
2022-08-04 18:44:41 +0000

    issue-3-users-can-view-liked first pass

--- js/data.js
+++ js/data.js
@@ -1,1 +1,25 @@
 /* exported data */
+var data = {
+  view: 'savedQuote',
+  entries: [],
+  editing: null,
+  nextEntryId: 1
+};
+
+var previousEntryJSON = localStorage.getItem('ajax-local-storage');
+if (previousEntryJSON !== null) {
+  data = JSON.parse(previousEntryJSON);
+}
+
+window.addEventListener('beforeunload', likedThought);
+
+function likedThought(event) {
+  var stringifyQuoteJSON = JSON.stringify(data);
+  localStorage.setItem('ajax-local-storage', stringifyQuoteJSON);
+}
+
+// window.addEventListener('beforeunload', saveJournal);
+// function saveJournal(event) {
+//   var stringifyJournalJSON = JSON.stringify(data);
+//   localStorage.setItem('ajax-local-storage', stringifyJournalJSON);
+// }
--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-var $quote = document.querySelector('#quote');
+var $quote = document.querySelector('.quote');
 var ul = document.querySelector('ul');
+var $button = document.querySelector('.button');
 
 function getData() {
   var xhr = new XMLHttpRequest();
@@ -21,23 +22,91 @@ function getData() {
 }
 getData();
 
-var likedThought = document.querySelector('#form-input');
 var text;
 var authorName;
 
-function handleClick(event) {
+function handleLike(event) {
   event.preventDefault();
 
   var savedQuote = {
     quote: text,
-    author: authorName
+    author: authorName,
+    entryId: data.nextEntryId
   };
 
   data.nextEntryId++;
-  likedThought.reset();
-  data.entries.unshift(likedThought);
-  ul.prepend(savedQuote);
+  // likedThought.reset();
+  data.entries.unshift(savedQuote);
+  renderLiked(savedQuote);
+  viewSwap(savedQuote);
+}
+
+if ($button != null) {
+  $button.addEventListener('click', handleLike);
+}
+
+function renderLiked(entry) {
+  var card = document.createElement('li');
+  // card.className('card');
+  card.setAttribute('class', 'card');
+
+  var text = document.createElement('li');
+  text.textContent = entry.text;
+
+  var person = document.createElement('li');
+  person.textContent = entry.person;
+
+  ul.appendChild(card);
+  ul.appendChild(text);
+  ul.appendChild(person);
 
 }
 
-likedThought.addEventListener('click', handleClick);
+var navLiked = document.querySelector('.nav-liked-thoughts');
+navLiked.addEventListener('click', viewSwap);
+
+function viewSwap(savedQuote) {
+  var parent = document.querySelector('.content');
+  parent.innerHTML = '';
+
+  var entries;
+  if ('quote' in savedQuote) {
+    entries = [savedQuote];
+  } else {
+    entries = data.entries;
+  }
+
+  for (var k = 0; k < entries.length; k++) {
+    if (entries[k] !== null) {
+      if (k % 2 === 0) {
+        var row = document.createElement('div');
+        row.setAttribute('class', 'row');
+        parent.append(row);
+      }
+      var columnHalf = document.createElement('div');
+      columnHalf.setAttribute('class', 'column-half');
+      row.append(columnHalf);
+      var card = document.createElement('div');
+      card.setAttribute('class', 'card');
+      columnHalf.append(card);
+      var quoteList = document.createElement('ul');
+      quoteList.setAttribute('class', 'quote');
+      card.append(quoteList);
+      var quote = document.createElement('li');
+      quoteList.append(quote);
+      var author = document.createElement('li');
+      quoteList.append(author);
+
+      quote.textContent = entries[k].quote;
+      author.textContent = entries[k].author;
+
+      if ('quote' in savedQuote) {
+        var newButton = document.createElement('button');
+        newButton.setAttribute('class', 'button');
+        newButton.setAttribute('type', 'submit');
+        newButton.textContent = 'LIKE';
+        card.append(newButton);
+      }
+    }
+  }
+}
